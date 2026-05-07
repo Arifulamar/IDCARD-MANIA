57d28defe863c4a736be869c53c1cbe12af9b431
Ariful Amar <arifulamar@Arifuls-MacBook-Pro.local>
2026-05-07 22:17:03 +0700

    Initial IDCARD commit

---
+++ public/next.config.js
@@ -0,0 +1,19 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',      // Sangat penting untuk GitHub Pages
+  images: {
+    unoptimized: true,   // Agar gambar muncul di GitHub Pages
+  },
+  // Jika nama repositori GitHub Anda bukan username.github.io (misal: my-store)
+  // Anda mungkin perlu menambahkan basepath:
+  // basePath: '/nama-repo-anda', 
+};
+
+module.exports = withPWA(nextConfig);
